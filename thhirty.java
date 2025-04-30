026d59deb0e84fe641e22b7fa42b724962f7a81a
Dharmendra <ds1489352@gmail.com>
2025-04-30 10:41:01 +0530

    done

---
+++ thhirty.java
@@ -0,0 +1,34 @@
+//Take a matrix as input from the user. search for the given number x and print the indices at which it occur.
+package Java;
+import java.util.*;
+
+public class thhirty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number of rows : ");
+		int rows = sc.nextInt();
+		System.out.print("Enter the number of cols : ");
+		int cols = sc.nextInt();
+		int [][] numbers= new int [rows][cols];
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<cols; j++) {
+				numbers[i][j]= sc.nextInt();
+			}
+			System.out.println();
+		}
+		System.out.print("Enter a value to search : ");
+		int x = sc.nextInt();
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<cols; j++) {
+				if(numbers[i][j]==x) {
+					System.out.print("value found at: " + i +", " + j);
+				}
+			}
+		}
+
+	}
+
+}
