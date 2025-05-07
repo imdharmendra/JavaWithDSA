de2c9fb7c92d63521476be34586015688c4bdf80
Dharmendra <147905152+imdharmendra@users.noreply.github.com>
2025-05-07 10:43:55 +0530

    Update thirtyfour.java

--- thirtyfour.java
+++ thirtyfour.java
@@ -10,8 +10,7 @@ public class thirtyfour {
 		   Scanner sc = new Scanner (System.in);
 		     String str = sc.next();
 		     String result = "";
-
-
+		
 		     for(int i=0; i<str.length(); i++) {
 		       if(str.charAt(i) == 'e') {
 		         result += 'i';
